1179c4c4e4a6ec5268641a7fdd9631377fa2ceeb
Igor Iric <iiric@microsfot.com>
2020-01-23 17:53:44 +0100

    first day commit

---
+++ cmd-learn/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+
+namespace cmd_learn
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            
+            //
+            // Create a root command and subcommands
+            //
+            var rootCommand = new RootCommand("CLI arguments learning app");
+            var monthCommand = new Command("getmonth", "gets information about current month");
+            var dayCommand = new Command("getday", "gets name of todays day")
+            {
+                new Option("--Tomorrow", "Gets information about tomorrow instead of today") {Argument = new Argument<bool>()}
+            };
+            
+            rootCommand.AddCommand(dayCommand);
+            rootCommand.AddCommand(monthCommand);
+
+            //
+            // add command handlers
+            //
+            rootCommand.Handler = CommandHandler.Create<bool>((boolOption) =>
+            {
+                Console.WriteLine("ahoj");
+                // TODO: Change this to description
+                Console.WriteLine(rootCommand.Description.ToString());
+                Console.WriteLine(rootCommand.ToString());
+            });
+
+            dayCommand.Handler = CommandHandler.Create<bool>((tomorrow) =>
+            {
+                if (tomorrow) {
+                    Console.WriteLine("Tomorrow is {0}", DateTime.Now.AddDays(1).DayOfWeek.ToString());
+                } else {
+                    Console.WriteLine("Today is {0}", DateTime.Now.DayOfWeek.ToString());
+                }
+            });
+
+            monthCommand.Handler = CommandHandler.Create(() =>
+            {
+                Console.WriteLine("Today is month {0}", DateTime.Now.ToString("MMMM"));
+            });
+
+
+            // Parse the incoming args and invoke the handler
+            return rootCommand.InvokeAsync(args).Result;
+        }
+    }
+}
---
+++ firstApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+
+namespace firstApp
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            // Create a root command with some options
+            var rootCommand = new RootCommand
+    {
+        new Option(
+            "--int-option",
+            "An option whose argument is parsed as an int")
+        {
+            Argument = new Argument<int>(defaultValue: () => 42)
+        },
+        new Option(
+            "--bool-option",
+            "An option whose argument is parsed as a bool")
+        {
+            Argument = new Argument<bool>()
+        },
+        new Option(
+            "--file-option",
+            "An option whose argument is parsed as a FileInfo")
+        {
+            Argument = new Argument<FileInfo>()
+        }
+    };
+
+            rootCommand.Description = "My sample app";
+
+            rootCommand.Handler = CommandHandler.Create<int, bool, FileInfo>((intOption, boolOption, fileOption) =>
+            {
+                Console.WriteLine($"The value for --int-option is: {intOption}");
+                Console.WriteLine($"The value for --bool-option is: {boolOption}");
+                Console.WriteLine($"The value for --file-option is: {fileOption?.FullName ?? "null"}");
+            });
+
+            // Parse the incoming args and invoke the handler
+            return rootCommand.InvokeAsync(args).Result;
+        }
+    }
+}
